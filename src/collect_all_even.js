1ca143e92dc16b9478b25fbf109c9043691d51bc
zhaolu <1632089314@qq.com>
2016-05-17 23:23:48 +0800

    finished

---
+++ src/collect_all_even.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function collect_all_even(collection) {
+
+    var newArray = [];
+
+    collection.forEach(function (element) {
+        if(element % 2 === 0) {
+            newArray.push(element);
+        }
+    });
+    
+    return newArray;
+}
+
+module.exports = collect_all_even;
